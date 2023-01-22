a3042e6b380b781065d2715c75b3e03b76c0a415
Sanuja Seneviratne <66342986+SanujaNS@users.noreply.github.com>
2023-01-22 11:02:32 +0530

    version bump

--- xmr.sh
+++ xmr.sh
@@ -7,7 +7,7 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
-cd xmrig-6.16.4
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
+tar xvzf xmrig-6.18.1-linux-x64.tar.gz
+cd xmrig-6.18.1
 ./xmrig -o gulf.moneroocean.stream:443 -u 42Tm1ydAcsKeAFAa5sdbFocXQGCJ8KPUx6moHhULy7NTVUgAPcxCSU8EQgiQUCvVFsTeZ9sqEf6vJjnMgsDYgSdLB4z4QgG -k --tls -p sanuja
